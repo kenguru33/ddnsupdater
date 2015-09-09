4b32a0835ed6d59cd8b062791baff0c1a5192552
bernt <bernt.anker@me.com>
2015-09-09 23:04:44 +0200

    Runnable Script name changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dnsupdater',
-    version='0.1',
+    version='0.2',
     packages= find_packages(),
     url='bernt-anker.com',
     license='GPLv3',
