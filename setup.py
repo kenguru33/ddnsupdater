bdbf7179c0cfb104e22f70094bb442255f1aa186
bernt <bernt.anker@me.com>
2015-09-13 00:16:42 +0200

    README update

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='dnsupdater',
     version='0.2',
-    packages= find_packages(),
+    packages=find_packages(),
     url='bernt-anker.com',
     license='GPLv3',
     author='Bernt Anker',
     author_email='bernt.anker@me.com',
     description='Dynamic DNS Updater',
-    install_requires=['pygodaddy',],
+    install_requires=['', ],
+    dependency_links=[
+        'https://github.com/claneys/pygodaddy/archive/master.zip',
+    ],
     entry_points={
         'console_scripts': [
             'gd-dnsupdater=dnsupdater:gd_dnsupdater',
